d7da0bbe1687ccdcbc9f3c1eb6d4509e1aec8514
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-06-06 12:04:56 +0000

    add associations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :likes, dependent: :destroy
-  has_many :comments, dependent: :destroy
-  has_many :posts, dependent: :destory
+  has_many :likes, foreign_key: :author_id, dependent: :destroy
+  has_many :comments, foreign_key: :author_id, dependent: :destroy
+  has_many :posts, foreign_key: :author_id, dependent: :destroy
 end
