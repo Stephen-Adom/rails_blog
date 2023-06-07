d69f968642168735cfc13bf3c854a10c22f14130
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-06-07 08:52:55 +0000

    rename comment_counter to comments_counter in posts table

---
+++ db/migrate/20230607084954_rename_comment_counter_in_post.rb
@@ -0,0 +1,5 @@
+class RenameCommentCounterInPost < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :posts, :comment_counter, :comments_counter
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_112929) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_084954) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,7 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_112929) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.integer "comment_counter"
+    t.integer "comments_counter"
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
