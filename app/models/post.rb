7cf2b2c2a17b45c027adc24f044238ba728e757e
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-06-07 09:41:21 +0000

    add method to update posts counter for a user

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments
   has_many :likes
+
+  scope :update_posts_counter, -> { User.find(3).update(posts_counter: User.find(3).posts.count) }
 end
