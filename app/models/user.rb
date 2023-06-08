f290c658ae117b9c8e40403f68cae0d07fadb2e8
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-06-08 09:40:14 +0000

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id, dependent: :destroy
   has_many :posts, foreign_key: :author_id, dependent: :destroy
 
+  validates :name, presence: true
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   scope :user_recent_posts, -> { find(2).posts.order(created_at: :desc).first(3) }
 end
