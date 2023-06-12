c03d9affa9bdd35dcb3d6244cb39315c99c4054b
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-06-12 09:14:43 +0000

    add placeholders to view files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,9 @@
 class PostsController < ApplicationController
+  def index
+    @placeholder = "List of posts for user id: #{params[:id]}"
+  end
+
+  def show
+    @placeholder = "List of post details for id: #{params[:post_id]} for user id: #{params[:id]}"
+  end
 end
