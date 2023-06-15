db84ff2bbf1c365d0c53d7641294d3e4d30ba504
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-06-15 12:59:50 +0000

    implement add comment for a post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(text: comment_params[:text], post_id: params[:post_id], author_id: current_user[:id])
+
+    if @comment.save
+      redirect_to user_posts_path(user_id: params[:user_id]), notice: 'Comment added successfully'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
 
+  validates :text, presence: true
+
   scope :update_comment_counter, -> { Post.find(3).update(comments_counter: Comment.where(post_id: 3).size) }
 end
