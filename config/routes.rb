142d44e110465d2fc1eb93fff71bb35b47cfad17
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-06-15 08:39:30 +0000

    update routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "users#index"
 
-  get 'users/', to: 'users#index', as: :users
-  get 'users/:id', to: 'users#show', as: :user_show
-  get 'users/:user_id/posts', to: 'posts#index', as: :user_posts
-  get 'users/:user_id/posts/:id', to: 'posts#show', as: :user_post_show
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
