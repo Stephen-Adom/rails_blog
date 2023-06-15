1eb3734d879b7639f3628049fa3d1a7cd973dbe2
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-06-15 12:02:50 +0000

    add create post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,28 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(title: post_params[:title], text: post_params[:text], author_id: current_user[:id],
+                     comments_counter: 0, likes_counter: 0)
+
+    if @post.save
+      redirect_to user_post_path(current_user, @post), notice: 'Post was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
     @post = User.find(params[:user_id]).posts.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root "users#index"
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :new, :show]
   end
+
+  post 'users/:user_id/posts', to: 'posts#create', as: :user_create_post
 end
